d8b7c85f82672aa754399e3d52bf452a804dc7e9
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-09-28 14:40:22 +0900

    style: Update source code to satisfy google styleguide

--- spring-mvc/src/main/java/com/example/springmvc/SpringMvcApplication.java
+++ spring-mvc/src/main/java/com/example/springmvc/SpringMvcApplication.java
@@ -3,11 +3,15 @@ package com.example.springmvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This class runs spring application.
+ */
+
 @SpringBootApplication
 public class SpringMvcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringMvcApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringMvcApplication.class, args);
+  }
 
 }
